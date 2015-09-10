8775bed78794e1e1b39752fbdde506983ab2e5e9
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-10 14:12:46 +0800

    add discounter.js

---
+++ src/discounter.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function Discounter () {
+	this.rate = [25, 20, 10, 5, 0];
+}
+
+Discounter.prototype.getBigestSave = function(bookCounts) {
+
+	var bookCountsBackup = [];
+	var saveMoney = [];
+	for(i=0; i<5; i++) {
+		saveMoney[i] = 0;
+	}
+
+	if(!bookCounts[0] && !bookCounts[0] && !bookCounts[0] && !bookCounts[0] && !bookCounts[0]) {
+		return 0;
+	}
+
+	bookCounts.sort(function(a, b) {
+		return b - a;
+	});
+
+	for(var i = 1; i <= 5; i++) {
+		if(bookCounts[5-i] >= 1) {
+			for(var k = 0; k < 5; k++) {
+				bookCountsBackup[k] = bookCounts[k];
+			}
+
+			for(var j = 0; j <= 5 - i; j++) {
+				bookCountsBackup[j] = bookCountsBackup[j] - 1;
+			}
+
+			saveMoney[i - 1] = (5 - i + 1) * 8 * this.rate[i - 1] + this.getBigestSave(bookCountsBackup);
+		}
+	}
+
+	return Math.max.apply(null, saveMoney);
+};
+
+module.exports = Discounter;
