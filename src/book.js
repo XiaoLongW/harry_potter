70a30e3732a3a33558a78103fa25135c2bfa51fe
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-10 14:06:56 +0800

    add book.js

---
+++ src/book.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function Book(bookid, name, price) {
+	this.bookid = bookid;
+	this.name = name;
+	this.price = price;
+}
+
+Book.find = function(bookid) {
+	var Util = require('./util.js');
+	var allBooks = Util.loadAllBooks();
+
+	var book;
+	allBooks.forEach(function (oneBook) {
+		if (oneBook.bookid == bookid) {
+			book = oneBook;
+		}
+	});
+	return book;
+};
+
+module.exports = Book;
