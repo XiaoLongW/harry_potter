72359897d0c0699f1cfaa33e80535e050d46f528
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-10 20:25:13 +0800

    modify main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 'use strict';
-var Pos = require('./pos');
-var Scanner = require('./scanner');
-var Cart = require('./cart');
-var Discounter = require('./discounter');
+var Pos = require('./module/pos');
+var Scanner = require('./module/scanner');
+var Basket = require('./module/basket');
+var Discounter = require('./module/discounter');
 
 function main(tags) {
-	var cart = new Cart;
-	var scanner = new Scanner;
-	var discounter = new Discounter;
-	var pos = new Pos(scanner, cart, discounter);
+	var basket = new Basket();
+	var scanner = new Scanner();
+	var discounter = new Discounter();
+	var pos = new Pos(scanner, basket, discounter);
 
 	pos.scan(tags);
 	var receipt = pos.printReceip();
