78098b7630b92e08be76084cc716badd18a54c61
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-10 14:11:18 +0800

    add util.js

---
+++ src/util.js
@@ -0,0 +1,18 @@
+'use strict';
+var Book = require('./book.js');
+
+function Utils() {
+
+}
+
+Utils.loadAllBooks = function() {
+	return [
+		new Book('1', '哈利波特-第一季', 8.00),
+		new Book('2', '哈利波特-第二季', 8.00),
+		new Book('3', '哈利波特-第三季', 8.00),
+		new Book('4', '哈利波特-第四季', 8.00),
+		new Book('5', '哈利波特-第五季', 8.00)
+	];
+};
+
+module.exports = Utils;
