0c0913f74d0d535f9cd543162803495cdf00f4da
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-13 08:33:25 +0800

    modify

--- src/module/basket.js
+++ src/module/basket.js
@@ -13,4 +13,28 @@ Basket.prototype.addBasketBook = function(newBasketBook) {
 	}
 };
 
+Basket.prototype.getCounts = function() {
+	var bookCounts = [];
+	for(var x=0;x<5;x++){
+		var count = 0;
+		this.basketBooks.forEach(function(oneBasketBook){
+			if(oneBasketBook.book.bookid == x+1){
+				count = oneBasketBook.count;
+			}
+		});
+		bookCounts[x] = count;
+	}
+
+	return bookCounts;
+};
+
+Basket.prototype.getTotalPay = function () {
+	var totalPay =  0;
+	this.basketBooks.forEach(function(oneBasketBook){
+		totalPay += oneBasketBook.book.price * oneBasketBook.count;
+	});
+
+	return totalPay;
+};
+
 module.exports = Basket;
